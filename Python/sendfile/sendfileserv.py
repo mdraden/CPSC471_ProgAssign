8f8606e653332cb41f44416038e4563f086d080e
Your Name <you@example.com>
2024-06-22 13:32:28 -0700

    Added send and receive functions and usecases for get and putt

--- Python/sendfile/sendfileserv.py
+++ Python/sendfile/sendfileserv.py
@@ -7,6 +7,9 @@
 
 import socket
 import subprocess
+import os
+import sys
+import threading
 
 # The port on which to listen
 listenPort = 21
@@ -30,7 +33,7 @@ welcomeSock.listen(1)
 def recvAll(sock: socket, numBytes):
 
 	# The buffer
-	recvBuff = ''
+	recvBuff = b''
 	
 	# Keep receiving till all is received
 	while recvBuff < str(numBytes):
@@ -46,7 +49,55 @@ def recvAll(sock: socket, numBytes):
 		recvBuff += str(tmpBuff)
 	
 	return recvBuff
-		
+
+def handle_client_control(clientSock, addr):
+	print("Accepted connection from client: ", addr)
+
+	while True:
+		command = clientSock.recv(1024)
+		if command.startswith("ls"):
+			files = os.llistdir('.')
+			files_list = '\n'.join(files)
+			clientSock.send(files_list.encode())
+		elif command.startswith("get"):
+			_, filename = command.split()
+			if os.path.exists(filename):
+				send_file(clientSock, filename)
+			else:
+				clientSock.send(b"ERROR: File not found")
+		elif command.startswith("put"):
+			_, filename = command.split()
+			receive_file(clientSock, filename)
+		elif command.startswith("quit"):
+			break
+
+	clientSock.close()
+
+def send_file(clientSock, filename):
+	fileSize = os.path.getsize(filename)
+	clientSock.send(f"{fileSize:<10}".encode())
+
+	with open(filename, 'rb') as file:
+		bytes_read = file.read(1024)
+		while bytes_read:
+			clientSock.send(bytes_read)
+			bytes_read = file.read(1024)
+
+def receive_file(clientSock, filename):
+	header = recvAll(clientSock, 10).decode().strip()
+	fileSize = int(header)
+
+	with open(filename, 'wb') as file:
+		bytes_recieved = 0
+		while bytes_recieved < fileSize:
+			bytes_read = clientSock.recv(1024)
+			if not bytes_read:
+				break
+			file.write(bytes_read)
+			bytes_recieved += len(bytes_read)
+
+
+
 # Accept connections forever
 while True:
 	
